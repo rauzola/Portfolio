205bace3d1f471643daee700be16f54ade6ba095
Raul - Anpad <raul_sigoli@hotmail.com>
2022-06-26 20:56:20 -0300

    Qualification AND Work

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import './main.js'
 
 function App() {
   return (
@@ -173,6 +174,283 @@ function App() {
             </div>
           </div>
         </section>
+        {/* <!-- =========== FIM About ========  --> */}
+
+
+        {/* <!-- =========== Qualification ========  --> */}
+        <section className="qualification section">
+          <h2 data-heading="Minha Jornada" className="section_title">Qualificação</h2>
+
+          <div className="qualification_container container grid">
+
+            <div className="education">
+              <h3 className="qualification_title">
+                <i className="uil uil-graduation-cap"></i>
+                Education
+              </h3>
+
+              <div className="timeline">
+
+                <div className="timeline_item">
+                  <div className="circle_dot"></div>
+                  <h3 className="timeline_title">Universidade Pitágoras Unopar Anhanguera</h3>
+                  <p className="timeline_text">Engenharia de Software</p>
+                  <span className="timeline_date"> <i className="uil uil-calendar-alt"></i>
+                    2021 - Presente
+                  </span>
+                </div>
+
+
+                <div className="timeline_item">
+                  <div className="circle_dot"></div>
+                  <h3 className="timeline_title">FullStack JavaScript</h3>
+                  <p className="timeline_text">Onebitcode</p>
+                  <span className="timeline_date"> <i className="uil uil-calendar-alt"></i>
+                    2022 - Presente
+                  </span>
+                </div>
+
+
+                <div className="timeline_item">
+                  <div className="circle_dot"></div>
+                  <h3 className="timeline_title">Análise de dados com Python</h3>
+                  <p className="timeline_text">Universidade Norte do Paraná</p>
+                  <span className="timeline_date"> <i className="uil uil-calendar-alt"></i>
+                    2022 - 2022
+                  </span>
+                </div>
+
+                <div className="timeline_item">
+                  <div className="circle_dot"></div>
+                  <h3 className="timeline_title">Trilha Conectar</h3>
+                  <p className="timeline_text">Rocketseat</p>
+                  <span className="timeline_date"> <i className="uil uil-calendar-alt"></i>
+                    2022 - 2022
+                  </span>
+                </div>
+
+
+                <div className="timeline_item">
+                  <div className="circle_dot"></div>
+                  <h3 className="timeline_title">Linguagem de Programação Python - Basico </h3>
+                  <p className="timeline_text">Fundação Bradesco</p>
+                  <span className="timeline_date"> <i className="uil uil-calendar-alt"></i>
+                    2022 - 2022
+                  </span>
+                </div>
+
+                <div className="timeline_item">
+                  <div className="circle_dot"></div>
+                  <h3 className="timeline_title">Coach e Liderança</h3>
+                  <p className="timeline_text">Universidade Norte do Paraná</p>
+                  <span className="timeline_date"> <i className="uil uil-calendar-alt"></i>
+                    2021 - 2021
+                  </span>
+                </div>
+
+                <div className="timeline_item">
+                  <div className="circle_dot"></div>
+                  <h3 className="timeline_title">Gestão de Metas e Indicadores de Performace</h3>
+                  <p className="timeline_text">Universidade Norte do Paraná</p>
+                  <span className="timeline_date"> <i className="uil uil-calendar-alt"></i>
+                    2021 - 2021
+                  </span>
+                </div>
+
+              </div>
+
+            </div>
+
+            <div className="experience">
+              <h3 className="qualification_title">
+                <i className="uil uil-suitcase-alt"></i>
+                Experience
+              </h3>
+
+              <div className="timeline">
+
+                <div className="timeline_item">
+                  <div className="circle_dot"></div>
+                  <h3 className="timeline_title">Nome Projeto Ou Empresa</h3>
+                  <p className="timeline_text">descrição do projeto ou empresa</p>
+                  <span className="timeline_date"> <i className="uil uil-calendar-alt"></i>
+                    2021 - Present
+                  </span>
+                </div>
+
+                <div className="timeline_item">
+                  <div className="circle_dot"></div>
+                  <h3 className="timeline_title">Nome Projeto Ou Empresa</h3>
+                  <p className="timeline_text">descrição do projeto ou empresa</p>
+                  <span className="timeline_date"> <i className="uil uil-calendar-alt"></i>
+                    2021 - Present
+                  </span>
+                </div>
+
+                <div className="timeline_item">
+                  <div className="circle_dot"></div>
+                  <h3 className="timeline_title">Nome Projeto Ou Empresa</h3>
+                  <p className="timeline_text">descrição do projeto ou empresa</p>
+                  <span className="timeline_date"> <i className="uil uil-calendar-alt"></i>
+                    2021 - Present
+                  </span>
+                </div>
+
+              </div>
+
+            </div>
+
+          </div>
+        </section>
+        {/* <!-- =========== FIM Qualification ========  --> */}
+
+        {/* <!-- =========== Work ========  --> */}
+        <section className="work section" id="work">
+          <h2 data-heading="Meu Portfólio" className="section_title">Projetos Recentes</h2>
+          <div className="work_filters">
+            <span className="work_item active-work" data-filter="all">Todos</span>
+            <span className="work_item" data-filter=".web">Web</span>
+            <span className="work_item" data-filter=".backend">Backend</span>
+            <span className="work_item" data-filter=".mobile">Mobile</span>
+          </div>
+
+          <div className="work_container container grid">
+
+
+            {/* <!-- PROJETOS 1 --> */}
+            <div className="work_card mix web">
+              <img src="https://portfolio-topaz-tau.vercel.app/assets/mockupPortfolio.png" alt="" className="work_img" />
+              <h3 className="work_title">Portfolio </h3>
+              <span className="work_button">Demo
+                <i className="uil uil-arrow-right work_button-icon"></i>
+              </span>
+              <div className="portfolio_item-details">
+                <h3 className="details_title">Projeto meu Portfolio</h3>
+                <p className="details_description">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus maiores
+                  exercitationem numquam.</p>
+                <ul className="details_info">
+                  <li>Criado - <span>19 Jun 2022</span></li>
+                  <li>Tecnologias - <span>html css javascript</span></li>
+                  <li>Função - <span>FrontEnd</span></li>
+                  <li>Visão - <span><a
+                    href="https://portfolio-topaz-tau.vercel.app/">https://portfolio-topaz-tau.vercel.app/</a></span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            {/* <!-- PROJETOS 2 --> */}
+            <div className="work_card mix web">
+              <img src="https://portfolio-topaz-tau.vercel.app/assets/em%20breve.webp" alt="" className="work_img" />
+              <h3 className="work_title">Site Cabelereiro</h3>
+              <span className="work_button">Demo
+                <i className="uil uil-arrow-right work_button-icon"></i>
+              </span>
+              <div className="portfolio_item-details">
+                <h3 className="details_title">Serviço feito para um salão</h3>
+                <p className="details_description">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus maiores
+                  exercitationem numquam.</p>
+                <ul className="details_info">
+                  <li>Criado - <span>19 Jun 2022</span></li>
+                  <li>Tecnologias - <span>html css javascript</span></li>
+                  <li>Função - <span>FrontEnd</span></li>
+                  <li>Visão - <span><a
+                    href="https://site-responsive-modelo-01.vercel.app/">https://site-responsive-modelo-01.vercel.app/</a></span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            {/* <!-- PROJETOS 3 --> */}
+            <div className="work_card mix web">
+              <img src="https://portfolio-topaz-tau.vercel.app/assets/em%20breve.webp" alt="" className="work_img" />
+              <h3 className="work_title">Quest Game</h3>
+              <span className="work_button">Demo
+                <i className="uil uil-arrow-right work_button-icon"></i>
+              </span>
+              <div className="portfolio_item-details">
+                <h3 className="details_title">Jogo de adivinhação de futuro</h3>
+                <p className="details_description">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus maiores
+                  exercitationem numquam.</p>
+                <ul className="details_info">
+                  <li>Criado - <span>19 Jun 2022</span></li>
+                  <li>Tecnologias - <span>html css javascript</span></li>
+                  <li>Função - <span>FrontEnd</span></li>
+                  <li>Visão - <span><a
+                    href="http://question-game-three.vercel.app/">http://question-game-three.vercel.app/</a></span></li>
+                </ul>
+              </div>
+            </div>
+
+            {/* <!-- PROJETOS 4 --> */}
+            <div className="work_card mix web">
+              <img src="https://portfolio-topaz-tau.vercel.app/assets/em%20breve.webp" alt="" className="work_img" />
+              <h3 className="work_title">Landing Page OAB</h3>
+              <span className="work_button">Demo
+                <i className="uil uil-arrow-right work_button-icon"></i>
+              </span>
+              <div className="portfolio_item-details">
+                <h3 className="details_title">landing Page com Pagina de Captura</h3>
+                <p className="details_description">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus maiores
+                  exercitationem numquam.</p>
+                <ul className="details_info">
+                  <li>Criado - <span>19 Jun 2022</span></li>
+                  <li>Tecnologias - <span>html css javascript mailchimp</span></li>
+                  <li>Função - <span>FrontEnd</span></li>
+                  <li>Visão - <span><a
+                    href="https://pagina-de-captura-oab.vercel.app/">https://pagina-de-captura-oab.vercel.app/</a></span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            {/* <!-- PROJETOS 5 --> */}
+            <div className="work_card mix backend mobile">
+              <img src="https://portfolio-topaz-tau.vercel.app/assets/em%20breve.webp" alt="" className="work_img" />
+              <h3 className="work_title">Em Breve</h3>
+              <span className="work_button">Demo
+                <i className="uil uil-arrow-right work_button-icon"></i>
+              </span>
+              <div className="portfolio_item-details">
+                <h3 className="details_title">Lorem ipsum dolor sit amet.</h3>
+                <p className="details_description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus maiores
+                  exercitationem numquam.</p>
+                <ul className="details_info">
+                  <li>Criado - <span>19 Jun 2022</span></li>
+                  <li>Tecnologias - <span>html css javascript</span></li>
+                  <li>Função - <span>FrontEnd</span></li>
+                  <li>Visão - <span><a href="#">em breve</a></span></li>
+                </ul>
+              </div>
+            </div>
+
+            {/* <!-- PROJETOS 6 --> */}
+            <div className="work_card mix backend mobile">
+              <img src="https://portfolio-topaz-tau.vercel.app/assets/em%20breve.webp" alt="" className="work_img" />
+              <h3 className="work_title">Em Breve</h3>
+              <span className="work_button">Demo
+                <i className="uil uil-arrow-right work_button-icon"></i>
+              </span>
+              <div className="portfolio_item-details">
+                <h3 className="details_title">Lorem ipsum dolor sit amet consectetur.</h3>
+                <p className="details_description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus maiores
+                  exercitationem numquam.</p>
+                <ul className="details_info">
+                  <li>Criado - <span>19 Jun 2022</span></li>
+                  <li>Tecnologias - <span>html css javascript</span></li>
+                  <li>Função - <span>FrontEnd</span></li>
+                  <li>Visão - <span><a href="#">em breve</a></span></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </section>
+        {/* <!-- =========== FIM Work ========  --> */}
+
       </main>
 
     </>
